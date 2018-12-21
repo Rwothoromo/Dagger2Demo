554402383bd088a661a0b8e1f52c11f83ccd055b
Rwothoromo Elijah <rwothoromoelijah@gmail.com>
2018-12-21 12:43:12 +0300

    Using the @Named qualifier to appoint someone to decide when to use what

--- app/src/main/java/com/example/rwothoromo/dagger2demo/MainActivity.kt
+++ app/src/main/java/com/example/rwothoromo/dagger2demo/MainActivity.kt
@@ -7,17 +7,26 @@ import dagger.Module
 import dagger.Provides
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+import javax.inject.Named
+
+const val LOVE = "Love"
+const val HELLO = "Hello"
 
 class MainActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var info: Info // Inform Dagger 2 which member variable it should create magically
+    @field:Named("Love") // @field is a Kotlin way of adding a field annotation when using it
+    lateinit var infoLove: Info // Inform Dagger 2 which member variable it should create magically
+
+    @Inject
+    @field:Named("Hello")
+    lateinit var infoHello: Info
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         DaggerSomethingComponent.create().inject(this)
-        text_view.text = info.text
+        text_view.text = "${infoLove.text} ${infoHello.text}"
     }
 }
 
@@ -33,5 +42,17 @@ class SomethingModule { // a bag of sorts to store a repository of provided obje
     @Provides
     fun providesInfo(): Info {
         return Info("This function is here to replace the text!")
+    }
+
+    @Provides
+    @Named(LOVE) // @Named is an inbuilt Qualifier, we can use a String to differentiate the Provider
+    fun sayLoveDagger2(): Info {
+        return Info("Love Dagger 2")
+    }
+
+    @Provides
+    @Named(HELLO)
+    fun sayHelloDagger2(): Info {
+        return Info("Hello Dagger 2")
     }
 }
